095d3c4bfd2a256d80d9fe1091c613d2839c9503
oozaikin <alex.zaikin@ticketvaultpos.com>
2020-03-23 16:18:35 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ declare let $: any;
 })
 export class AppComponent {
   name = 'Angular';
+  mainDocHead : JQuery<any>;
 
   ngOnInit() {
-    
+    this.mainDocHead = jQuery('script[src*="dt-1.10.9/datatables.min.js"]').parent();
     // this.loadCss('../app/assets/dataTables.checkboxes.css');
     //  this.loadScript('../app/assets/dataTables.checkboxes_CUSTOM.js');
      this.loadCss('https://cdn.datatables.net/select/1.3.1/css/select.dataTables.css');
@@ -18,22 +19,23 @@ export class AppComponent {
      this.loadScript('https://cdn.datatables.net/select/1.3.1/js/dataTables.select.js');
      this.loadScript('https://cdn.datatables.net/scroller/2.0.1/js/dataTables.scroller.js');
      this.loadScript('https://cdn.datatables.net/rowgroup/1.1.1/js/dataTables.rowGroup.js');
+     //this.mainDocHead.parent().parent()[0].location.reload() 
   }  
 
   public loadScript(url: string) {
     
-    const dtMainScript = jQuery('script[src*="dt-1.10.9/datatables.min.js"]').parent();
+    
     const script = document.createElement('script');
     script.innerHTML = '';
     script.src = url;
     //script.async = false;
     //script.defer = true;
-    dtMainScript.append(script.outerHTML);
+    this.mainDocHead.append(script.outerHTML);
+    
   }
 
-  public loadCss(url: string) {
-    
-    const dtMainScript = jQuery('script[src*="dt-1.10.9/datatables.min.js"]').parent();    
+  public loadCss(url: string) {    
+      
     var link = document.createElement('link'); 
   
         // set the attributes for link element  
@@ -43,7 +45,7 @@ export class AppComponent {
       
         link.href = url; 
     
-    dtMainScript.append(link.outerHTML);
+    this.mainDocHead.append(link.outerHTML);
   }
 
 }
