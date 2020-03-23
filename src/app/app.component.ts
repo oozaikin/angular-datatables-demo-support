e6e93a8f40f101bdc5945e019a4a004c617f1ec1
oozaikin <alex.zaikin@ticketvaultpos.com>
2020-03-23 15:14:43 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,37 @@ export class AppComponent {
   name = 'Angular';
 
   ngOnInit() {
+    
+    this.loadCss('../app/assets/dataTables.checkboxes.');
      this.loadScript('../app/assets/dataTables.checkboxes_CUSTOM.js');
+     this.loadCss('https://cdn.datatables.net/select/1.3.1/css/select.dataTables.css');
      this.loadScript('https://cdn.datatables.net/select/1.3.1/js/dataTables.select.js');
   }  
 
   public loadScript(url: string) {
     const body = <HTMLDivElement> document.body;
+    const dtMainScript = jQuery('script[src*="dt-1.10.9/datatables.min.js"]').parent();
     const script = document.createElement('script');
     script.innerHTML = '';
     script.src = url;
-    script.async = false;
-    script.defer = true;
-    body.appendChild(script);
+    //script.async = false;
+    //script.defer = true;
+    dtMainScript.append(script.outerHTML);
   }
 
+  public loadCss(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const dtMainScript = jQuery('script[src*="dt-1.10.9/datatables.min.js"]').parent();    
+    var link = document.createElement('link'); 
+  
+        // set the attributes for link element  
+        link.rel = 'stylesheet';  
+      
+        link.type = 'text/css'; 
+      
+        link.href = url; 
+    
+    dtMainScript.append(link.outerHTML);
+  }
 
 }
